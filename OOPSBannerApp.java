0833e634b71824c71a07988d91ccd2cdf690b1df
Jebin V <v.jebinff@gmail.com>
2026-03-03 09:47:58 +0530

    UC6 - Refactored banner using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,58 @@
-/**
- * OOPS Banner App
- * UC4: Render OOPS as Banner using String Array and Loop
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
+        String[] banner = {
+            String.join(" ", getO()[0], getO()[0], getP()[0], getS()[0]),
+            String.join(" ", getO()[1], getO()[1], getP()[1], getS()[1]),
+            String.join(" ", getO()[2], getO()[2], getP()[2], getS()[2]),
+            String.join(" ", getO()[3], getO()[3], getP()[3], getS()[3]),
+            String.join(" ", getO()[4], getO()[4], getP()[4], getS()[4]),
+            String.join(" ", getO()[5], getO()[5], getP()[5], getS()[5]),
+            String.join(" ", getO()[6], getO()[6], getP()[6], getS()[6])
+        };
 
-        banner[0] = String.join(" ", " ***** ", " ***** ", " ***** ", " ***** ");
-        banner[1] = String.join(" ", "*     *", "*     *", "*     *", "*     *");
-        banner[2] = String.join(" ", "*     *", "*     *", "*     *", "*      ");
-        banner[3] = String.join(" ", "*     *", "*     *", " ***** ", " ***** ");
-        banner[4] = String.join(" ", "*     *", "*     *", "*      ", "     * ");
-        banner[5] = String.join(" ", "*     *", "*     *", "*      ", "     * ");
-        banner[6] = String.join(" ", " ***** ", " ***** ", "*      ", " ***** ");
-
-        // Enhanced for loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Helper method for O
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Helper method for P
+    static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Helper method for S
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
     }
 }
